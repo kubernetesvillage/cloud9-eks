e9efc2c029d0a1dc9ae3403a9386d9174d52582e
Divyanshu <128726404+s3curitydojolab@users.noreply.github.com>
2024-01-14 03:15:10 +0530

    Update file.sh

--- scripts/file.sh
+++ scripts/file.sh
@@ -76,9 +76,9 @@ download_and_verify "https://github.com/mikefarah/yq/releases/download/v${yq_ver
 chmod +x ./yq
 mv ./yq /usr/local/bin
 
-# terraform using Yum
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && yum makecache fast
-yum -y install terraform
+# # terraform using Yum
+# yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && yum makecache fast
+# yum -y install terraform
 
 # ec2 instance selector
 download_and_verify "https://github.com/aws/amazon-ec2-instance-selector/releases/download/v${ec2_instance_selector_version}/ec2-instance-selector-linux-amd64" "$ec2_instance_selector_checksum" "ec2-instance-selector-linux-amd64"
